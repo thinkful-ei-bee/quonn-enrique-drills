dc0ffaecb8cf6d5e666e42a77c43efd7cab1a4ba
Enrique Montemayor <eemontemayor@live.com>
2019-03-20 11:08:52 -0500

    helloWorld version 2

--- react-drills/src/state-drills/HelloWorld.js
+++ react-drills/src/state-drills/HelloWorld.js
@@ -5,16 +5,29 @@ class HelloWorld extends React.Component {
     constructor(props) { 
         super(props);
         this.state = { who: 'World'}
-     
+        this.handleClick = (newString) => {
+            this.setState({
+                who: newString
+            });
+        }
     }
  
    
+    // render() { 
+    //     return <div><p>Hello {this.state.who}</p>
+    //     <button onClick= {()=> this.setState({who: 'Friend'})}>Friend</button>
+    //     <button onClick= {()=> this.setState({who: 'React'})}>React</button>
+    //     <button onClick= {()=> this.setState({who: 'World'})}>World</button>
+    //     </div>
+    // }
+
     render() { 
-        return <div><p>Hello {this.state.who}</p>
-        <button onClick= {()=> this.setState({who: 'Friend'})}>Friend</button>
-        <button onClick= {()=> this.setState({who: 'React'})}>React</button>
-        <button onClick= {()=> this.setState({who: 'World'})}>World</button>
+        return (<div><p>Hello {this.state.who}</p>
+        <button onClick= {()=> this.handleClick('Friend')}>Friend</button>
+        <button onClick= {()=> this.handleClick('React')}>React</button>
+        <button onClick= {()=> this.handleClick('World')}>World</button>
         </div>
+        )
     }
 
    
