bfb3b0aca77e35a6455e35ad4e021b32959489c0
Enrique Montemayor <eemontemayor@live.com>
2019-03-20 14:10:50 -0500

    finished state drills

--- react-drills/src/state-drills/RouletteGun.js
+++ react-drills/src/state-drills/RouletteGun.js
@@ -24,24 +24,24 @@ export default class RouletteGun extends React.Component {
         }, 2000 )
 }
 
-// renderDisplay() {
-//     const { chamber, spinningTheChamber } = this.state
-//     const { bulletInChamber } = this.props
-//     if (spinningTheChamber) {
-//       return 'spinning the chamber and pulling the trigger! ...'
-//     } else if (chamber === bulletInChamber) {
-//       return 'BANG!!!!!'
-//     } else {
-//       return 'you\'re safe!'
-//     }
-//   }
+renderDisplay() {
+    const { chamber, spinningTheChamber } = this.state
+    const { bulletInChamber } = this.props
+    if (spinningTheChamber) {
+      return 'spinning the chamber and pulling the trigger! ...'
+    } else if (chamber === bulletInChamber) {
+      return 'BANG!!!!!'
+    } else {
+      return 'you\'re safe!'
+    }
+  }
 
 
 
     render() { 
         return (
         <div className='RouletteGun'>
-        {/* <p>{this.renderDisplay()}</p> */}
+        <p>{this.renderDisplay()}</p>
         <button onClick={()=>this.handleClick()}>
           Pull the trigger!
         </button>
